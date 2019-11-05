bf91ba4f2b0367bbc6b5c6035fc60dbe766cf089
Arthur <arthur.w.zhen@gmail.com>
2019-11-05 17:31:56 +0800

    Update Error Files

--- client/src/App.vue
+++ client/src/App.vue
@@ -8,6 +8,10 @@
 <script>
 export default {
   name: 'App'
+}
+</script>
+
+<style>
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -15,9 +19,5 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
-}
+  }
 </style>
-}
-</script>
-
-<style>
